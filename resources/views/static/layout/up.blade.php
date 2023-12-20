0f71a7da8eca1cdcc9b60158dbd5c2928a2faa89
A H ANAM <anamkhanbd2@gmail.com>
2023-12-20 11:54:08 +0600

    2nd push

--- resources/views/static/layout/up.blade.php
+++ resources/views/static/layout/up.blade.php
@@ -2,12 +2,115 @@
 <html lang="en">
   <head>
     <title>Title</title>
-    <!-- Required meta tags -->
+    {{-- Required meta tags --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    {{-- Bootstrap CSS --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    {{--Font Awesome 5.12.0--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    {{--custom css --}}
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-      
+      <div class="container-fluid">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6" id="logo">
+                        <h1>
+                            <i class="fas fa-camera-retro"></i>
+                            Camera World
+                        </h1>
+                    </div>
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <strong>Toll Free : +8801505050505</strong> <br>
+                                <a href="#">Login</a>
+                                <a href="#">My Account</a>
+                                <a href="#">Card</a>
+                            </div>
+                            <div class="col-6">
+                                <input type="search" name="" id="" placeholder="Search Here "> 
+                                <i class="fas fa-search"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container">
+                <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                    {{--
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    --}}
+                    <button class="navbar-toggler d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Camera</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Flim Camera</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">DSLR Camera</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Lans</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Camera Bag</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Tripots</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Camera Battery</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Charger </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Microw Phone</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Software</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Gear</a>
+                            </li>
+                            {{--
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="#">Action 1</a>
+                                    <a class="dropdown-item" href="#">Action 2</a>
+                                </div>
+                            </li>
+                            --}}
+
+                        </ul>
+
+                        {{--
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                        --}}
+                    </div>
+                </nav>
+            </div>
+        </div>
+      </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div id="hero">
-                    <h1 class="text-center">Hello Mia.K</h1>
-                </div>
+                
             </div>
         </div>
     </div>
